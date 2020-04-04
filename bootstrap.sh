ec4042f9fe1852d91303b69094a9282145953b17
Jay Lub <jaylub@Jays-Mac-mini.local>
2020-04-04 18:06:11 -0500

    removed: unused parts of script

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,14 +8,6 @@ main() {
     install_homebrew
     install_packages_with_brewfile
     install_dotfiles
-
-    # change_shell_to_fish
-    # install_oh_my_fish
-    # install_pip_packages
-    # install_yarn_packages
-    # setup_symlinks # needed for setup_vim and setup_tmux
-    # setup_vim
-    # setup_tmux
     # update_hosts_file
     # setup_macOS_defaults
     # update_login_items
@@ -186,143 +178,6 @@ function pull_latest() {
     fi
 }
 
-function change_shell_to_fish() {
-    info "Fish shell setup"
-    if grep --quiet fish <<< "$SHELL"; then
-        success "Fish shell already exists"
-    else
-        user=$(whoami)
-        substep "Adding Fish executable to /etc/shells"
-        if grep --fixed-strings --line-regexp --quiet \
-            "/usr/local/bin/fish" /etc/shells; then
-            substep "Fish executable already exists in /etc/shells"
-        else
-            if echo /usr/local/bin/fish | sudo tee -a /etc/shells > /dev/null;
-            then
-                substep "Fish executable successfully added to /etc/shells"
-            else
-                error "Failed to add Fish executable to /etc/shells"
-                exit 1
-            fi
-        fi
-        substep "Switching shell to Fish for \"${user}\""
-        if sudo chsh -s /usr/local/bin/fish "$user"; then
-            success "Fish shell successfully set for \"${user}\""
-        else
-            error "Please try setting Fish shell again"
-        fi
-    fi
-}
-
-function install_oh_my_fish() {
-    info "Oh My Fish! setup"
-    if fish --command=omf &>/dev/null; then
-        success "Oh My Fish! already exists"
-    else
-        if fish --command="curl --silent --location https://get.oh-my.fish | fish /dev/stdin --yes"; then
-            success "Oh My Fish! installation succeeded"
-        else
-            error "Oh My Fish! installation failed"
-            exit 1
-        fi
-    fi
-}
-
-function install_pip_packages() {
-    info "Installing pip packages"
-    REQUIREMENTS_FILE=${DOTFILES_REPO}/pip/requirements.txt
-
-    if pip3 install -r "$REQUIREMENTS_FILE" 1> /dev/null; then
-        success "pip packages successfully installed"
-    else
-        error "pip packages installation failed"
-        exit 1
-    fi
-
-}
-
-function install_yarn_packages() {
-    # prettier for Neoformat to auto-format files
-    # typescript for YouCompleteMe
-    yarn_packages=(prettier typescript vmd create-react-app gatsby-cli netlify-cli all-contributors-cli)
-    info "Installing yarn packages \"${yarn_packages[*]}\""
-
-    yarn_list_outcome=$(yarn global list)
-    for package_to_install in "${yarn_packages[@]}"
-    do
-        if echo "$yarn_list_outcome" | \
-            grep --ignore-case "$package_to_install" &> /dev/null; then
-            substep "\"${package_to_install}\" already exists"
-        else
-            if yarn global add "$package_to_install"; then
-                substep "Package \"${package_to_install}\" installation succeeded"
-            else
-                error "Package \"${package_to_install}\" installation failed"
-                exit 1
-            fi
-        fi
-    done
-
-    success "yarn packages successfully installed"
-}
-
-function setup_vim() {
-    info "Setting up vim"
-    substep "Installing Vundle"
-    if test -e ~/.vim/bundle/Vundle.vim; then
-        substep "Vundle already exists"
-        pull_latest ~/.vim/bundle/Vundle.vim
-        substep "Pull successful in Vundle's repository"
-    else
-        url=https://github.com/VundleVim/Vundle.vim.git
-        if git clone "$url" ~/.vim/bundle/Vundle.vim; then
-            substep "Vundle installation succeeded"
-        else
-            error "Vundle installation failed"
-            exit 1
-        fi
-    fi
-    substep "Installing all plugins"
-    if vim +PluginInstall +qall 2> /dev/null; then
-        substep "Plugins installations succeeded"
-    else
-        error "Plugins installations failed"
-        exit 1
-    fi
-    success "vim successfully setup"
-}
-
-function setup_tmux() {
-    info "Setting up tmux"
-    substep "Installing tpm"
-    if test -e ~/.tmux/plugins/tpm; then
-        substep "tpm already exists"
-        pull_latest ~/.tmux/plugins/tpm
-        substep "Pull successful in tpm's repository"
-    else
-        url=https://github.com/tmux-plugins/tpm
-        if git clone "$url" ~/.tmux/plugins/tpm; then
-            substep "tpm installation succeeded"
-        else
-            error "tpm installation failed"
-            exit 1
-        fi
-    fi
-
-    substep "Installing all plugins"
-
-    # sourcing .tmux.conf is necessary for tpm
-    tmux source-file ~/.tmux.conf 2> /dev/null
-
-    if ~/.tmux/plugins/tpm/bin/./install_plugins &> /dev/null; then
-        substep "Plugins installations succeeded"
-    else
-        error "Plugins installations failed"
-        exit 1
-    fi
-    success "tmux successfully setup"
-}
-
 function setup_symlinks() {
     APPLICATION_SUPPORT=~/Library/Application\ Support
     POWERLINE_ROOT_REPO=/usr/local/lib/python3.7/site-packages
@@ -472,21 +327,6 @@ function update_hosts_file() {
 
     else
         error "Failed to download hosts file"
-        exit 1
-    fi
-}
-
-function setup_macOS_defaults() {
-    info "Updating macOS defaults"
-
-    current_dir=$(pwd)
-    cd ${DOTFILES_REPO}/macOS
-    if bash defaults.sh; then
-        cd $current_dir
-        success "macOS defaults updated successfully"
-    else
-        cd $current_dir
-        error "macOS defaults update failed"
         exit 1
     fi
 }
