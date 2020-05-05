2110d75ad73bf7c55c59b80fd09cc11c5f53fe4a
Jay Lub <undeadlegion@gmail.com>
2020-05-05 15:55:42 -0500

    added: vscode settings

---
+++ Code/install.sh
@@ -0,0 +1,6 @@
+
+pwd
+cat vs_code_extensions_list.txt | xargs -n 1 code --install-extension
+ln -s ./keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
+ln -s ./settings.json ~/Library/Application\ Support/Code/User/settings.json
+ln -s ./keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
